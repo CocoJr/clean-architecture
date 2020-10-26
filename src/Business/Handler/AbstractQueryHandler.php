a9dfecf59c48cd070d1aa1dd91a7dd5ca9c7263c
Thibault C <thibaultcolette06@gmail.com>
2020-10-26 15:48:03 +0100

    Add EventDispatcher and Repository dependencies for QueryHandler and CommandHandler

--- src/Business/Handler/AbstractQueryHandler.php
+++ src/Business/Handler/AbstractQueryHandler.php
@@ -2,6 +2,17 @@
 
 namespace CocoJr\CleanArchitecture\Business\Handler;
 
+use CocoJr\CleanArchitecture\Business\Service\EventDispatcherInterface;
+use CocoJr\CleanArchitecture\Domain\Repository\RepositoryInterface;
+
 abstract class AbstractQueryHandler
 {
+    protected EventDispatcherInterface $eventDispatcher;
+    protected RepositoryInterface $repository;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, RepositoryInterface $repository)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->repository = $repository;
+    }
 }
