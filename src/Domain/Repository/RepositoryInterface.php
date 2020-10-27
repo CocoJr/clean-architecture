4fdbfadab34037eb1165ac9b8016181e687b3e54
Thibault C <thibaultcolette06@gmail.com>
2020-10-27 12:56:41 +0100

    Enhance RepositoryInterface

--- src/Domain/Repository/RepositoryInterface.php
+++ src/Domain/Repository/RepositoryInterface.php
@@ -7,4 +7,5 @@ use CocoJr\CleanArchitecture\Domain\Entity\EntityInterface;
 interface RepositoryInterface
 {
     public function getNewEntity(): EntityInterface;
+    public function save(EntityInterface $entity): void;
 }
